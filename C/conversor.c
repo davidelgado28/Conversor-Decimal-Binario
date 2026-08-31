6cf76a6786f76f392d4db6b7c8380b640c4c43df
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-31 10:46:25 -0300

    Add decimal to binary converter in conversor.c

---
+++ C/conversor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int decimal, numero, i = 0;
+    char binario[64];
+
+    printf("Digite um numero decimal: ");
+   ("%d", &decimal);
+
+    if(decimal==0){
+        printf("Binario: 0\n");
+        return 0;
+    }
+    numero = decimal;
+    while(numero>0){
+        binario[i++] = (numero % 2) + '0';
+        numero /= 2;
+    }
+    binario[i] = '\0';
+
+    printf("Binario: ");
+    for(int j = i-1; j>=0; j--){
+        printf("%c", binario[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
